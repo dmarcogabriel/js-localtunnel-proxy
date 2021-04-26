a114de73bc99ba180f3db96337725652c755444b
Gabriel Lino de Marco <biel_dmarco@hotmail.com>
2021-04-26 12:21:39 -0300

    add project base structure

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 12,
+  },
+  'rules': {
+    'arrow-parens': ['error', 'as-needed'],
+    'semi': ['error', 'never'],
+  },
+}
